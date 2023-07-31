6ff2e6d23e6585836d5083a86c622e446f975393
25th-Night <brown_eyed87@naver.com>
2023-07-31 22:39:13 +0900

    account/urls: django auth의 LogoutView로 다시 변경

--- account/urls.py
+++ account/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
 
     # 새로운 로그인 / 로그아웃 URL 패턴
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('logout/', views.user_logout, name='logout'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    # path('logout/', views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 
     # 비밀번호 변경 URL 패턴
