619ae451542fe24785f93cc7a4736e086c42177e
25th-Night <brown_eyed87@naver.com>
2023-07-31 20:59:50 +0900

    account/views - dashboard 뷰 추가

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
@@ -43,3 +44,8 @@ class LoginView(FormView):
                 return HttpResponse('Disable account')
         else:
             return HttpResponse('Invalid login')
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
