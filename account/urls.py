3d935f94e50bc65916824d870834773d257aba8c
25th-Night <brown_eyed87@naver.com>
2023-07-31 21:44:58 +0900

    account/urls: 비밀번호 초기화 관련 url 패턴 등록

--- account/urls.py
+++ account/urls.py
@@ -4,20 +4,36 @@ from . import views
 
 
 urlpatterns = [
+    # 이전 로그인 URL 패턴
     # path('login/', views.user_login, name='login'),
     # path('login/', views.LoginView.as_view(), name='login'),
 
-    # new login and logout views
+    # 새로운 로그인 / 로그아웃 URL 패턴
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 
-    # 비밀번호 변경 url
+    # 비밀번호 변경 URL 패턴
     path('password-change/',
         auth_views.PasswordChangeView.as_view(),
         name='password_change'),
     path('password-change/done/',
         auth_views.PasswordChangeDoneView.as_view(),
         name='password_change_done'),
+    path('', views.dashboard, name='dashboard'),
+
+    # 배밀번호 초기화 URL 패턴
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('password-reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
