5c0ad55164c67d4a1174e78fbedc639503477ce9
25th-Night <brown_eyed87@naver.com>
2023-07-31 21:34:44 +0900

    account/urls: password change 관련 뷰 등록

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,13 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+
+    # 비밀번호 변경 url
+    path('password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    path('password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
+    path('', views.dashboard, name='dashboard'),
 ]
