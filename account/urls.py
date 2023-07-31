1ce1cd9f47e8071f47ed50a15a993e08464bbdf9
25th-Night <brown_eyed87@naver.com>
2023-07-31 23:21:56 +0900

    account/urls: CustomLoginView를 url에 추가

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
-
+from .views import CustomLoginView
 
 urlpatterns = [
     # 이전 로그인 / 로그아웃 URL 패턴
@@ -10,6 +10,7 @@ urlpatterns = [
     # path('logout/', views.user_logout, name='logout'),
     # path('logout/', views.LogoutView.as_view(), name='logout'),
 
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     # path('register/', views.register, name='register'),
