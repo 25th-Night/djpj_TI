c2d94efc5389925b6452f48fe0db8e1f2ad43af3
25th-Night <brown_eyed87@naver.com>
2023-07-31 23:03:52 +0900

    account/views: Register 뷰 FBV/CBV 추가

--- account/views.py
+++ account/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import FormView
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -64,3 +64,47 @@ class LogoutView(View):
         logout(request)
         return render(request, 'account/logged_out.html')
 
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # 새로운 사용자 객체를 생성하지만 아직 저장하지는 않습니다.
+            new_user = user_form.save(commit=False)
+            # 선택한 비밀번호를 설정합니다.
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # 사용자 객체를 저장합니다.
+            new_user.save()
+            return render(request,
+                            'account/register_done.html',
+                            {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                    'account/register.html',
+                    {'user_form': user_form})
+
+
+class RegisterView(FormView):
+    template_name = "account/register.html"
+    form_class = UserRegistrationForm
+    success_url = "account/register_done.html"
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+        new_user = form.save(commit=False)
+        new_user.set_password(cd['password'])
+        new_user.save()
+        return render(self.request, self.get_success_url(), {'new_user': new_user})
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = context.get('form')
+        return context
+
+    def get_success_url(self):
+        return self.success_url
