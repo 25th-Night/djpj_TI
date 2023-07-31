a25cfc5381bc2838ff238ddef0da75c628c53d05
25th-Night <brown_eyed87@naver.com>
2023-07-31 21:01:22 +0900

    config/settings: 로그인/로그아웃 관련 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -176,3 +176,9 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = secrets["GMAIL"]["default"]["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = secrets["GMAIL"]["default"]["EMAIL_HOST_PASSWORD"]
+
+
+# login settings
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
