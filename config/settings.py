8a9d8d37e07b55350f71629f7cb8b0d333976595
25th-Night <brown_eyed87@naver.com>
2023-08-04 01:36:28 +0900

    config/settings: 소셜 인증 파이프라인 추가

--- config/settings.py
+++ config/settings.py
@@ -215,3 +215,16 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 # 구글 인증
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = secrets["SOCIAL_AUTH_GOOGLE_OAUTH2_KEY"]
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = secrets["SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET"]
+
+# 소셜 인증 파이프라인 추가
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
