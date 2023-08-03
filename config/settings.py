92c40d78315f8d9a7f2e1ddee85b0eb3afdb8009
25th-Night <brown_eyed87@naver.com>
2023-08-04 01:34:06 +0900

    config/settings: 구글 인증 관련 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -203,6 +203,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 # 페이스북 인증
@@ -210,3 +211,7 @@ SOCIAL_AUTH_FACEBOOK_KEY = secrets["SOCIAL_AUTH_FACEBOOK_KEY"]
 SOCIAL_AUTH_FACEBOOK_SECRET = secrets["SOCIAL_AUTH_FACEBOOK_SECRET"]
 
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+# 구글 인증
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = secrets["SOCIAL_AUTH_GOOGLE_OAUTH2_KEY"]
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = secrets["SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET"]
