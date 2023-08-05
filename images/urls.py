3c973e58324d1eb8d718ba809a7ebd9f75a03954
25th-Night <brown_eyed87@naver.com>
2023-08-05 22:34:56 +0900

    images/urls: ImageListView url 추가

--- images/urls.py
+++ images/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('create/', views.ImageCreateView.as_view(), name='create'),
     # path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
     path('detail/<int:id>/<slug:slug>/', views.ImageDetailView.as_view(), name='detail'),
-    path('like/', views.image_like, name='like'),
+    # path('like/', views.image_like, name='like'),
     path('like/', views.ImageLikeView.as_view(), name='like'),
+    # path('', views.image_list, name='list'),
+    path('', views.ImageListView.as_view(), name='list'),
 ]
