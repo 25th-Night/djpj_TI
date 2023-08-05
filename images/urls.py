7afcadf94f43078702632b87a7ed7491b95eebb7
25th-Night <brown_eyed87@naver.com>
2023-08-05 22:08:25 +0900

    images/urls: ImageLikeView url 등록

--- images/urls.py
+++ images/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', views.ImageCreateView.as_view(), name='create'),
     # path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
     path('detail/<int:id>/<slug:slug>/', views.ImageDetailView.as_view(), name='detail'),
+    path('like/', views.image_like, name='like'),
+    path('like/', views.ImageLikeView.as_view(), name='like'),
 ]
