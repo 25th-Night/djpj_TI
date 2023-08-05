0cc14f2bc2b9992ad71fb34a1fc04b4436ff797f
25th-Night <brown_eyed87@naver.com>
2023-08-05 23:06:05 +0900

    images/urls: slug 필드 한글 사용으로 인한 url path 방식을 repath로 변경

--- images/urls.py
+++ images/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'images'
@@ -7,7 +7,8 @@ urlpatterns = [
     # path('create/', views.image_create, name='create'),
     path('create/', views.ImageCreateView.as_view(), name='create'),
     # path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
-    path('detail/<int:id>/<slug:slug>/', views.ImageDetailView.as_view(), name='detail'),
+    # path('detail/<int:id>/<slug:slug>/', views.ImageDetailView.as_view(), name='detail'),
+    re_path(r'^(?P<id>[0-9]{1,})/(?P<slug>[-\w]+)/$', views.ImageDetailView.as_view(), name='detail'),
     # path('like/', views.image_like, name='like'),
     path('like/', views.ImageLikeView.as_view(), name='like'),
     # path('', views.image_list, name='list'),
