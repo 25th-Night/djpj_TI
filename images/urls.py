14af8a53bc8ddce1fe9bab6de84a767b1a469a32
25th-Night <brown_eyed87@naver.com>
2023-08-05 21:49:16 +0900

    images/urls: 이미지 상세 보기 url 등록

--- images/urls.py
+++ images/urls.py
@@ -6,4 +6,6 @@ app_name = 'images'
 urlpatterns = [
     # path('create/', views.image_create, name='create'),
     path('create/', views.ImageCreateView.as_view(), name='create'),
+    # path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('detail/<int:id>/<slug:slug>/', views.ImageDetailView.as_view(), name='detail'),
 ]
