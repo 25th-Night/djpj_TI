cd963d57da8998364606bfd8705ad5ee0f420161
25th-Night <brown_eyed87@naver.com>
2023-08-05 18:34:54 +0900

    images/models: ManyToManyField 추가 (즐겨찾기 한 사용자)

--- images/models.py
+++ images/models.py
@@ -13,14 +13,17 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         indexes = [
