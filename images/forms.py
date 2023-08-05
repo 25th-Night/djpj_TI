fb3bf63b4001a65bd9893085768b9f4e0fa72b0e
25th-Night <brown_eyed87@naver.com>
2023-08-05 18:40:55 +0900

    images/forms: 확장자 유효성 검증 로직 추가

--- images/forms.py
+++ images/forms.py
@@ -3,6 +3,15 @@ from .models import Image
 
 
 class ImageCreateForm(forms.ModelForm):
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
+
     class Meta:
         model = Image
         fields = ['title', 'url', 'description']
