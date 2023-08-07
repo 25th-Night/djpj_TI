3d958ac5b6fa1e69f486929cf061c5eec8c3e111
25th-Night <brown_eyed87@naver.com>
2023-08-07 21:08:50 +0900

    account/urls: UserList, UserDetail 뷰를 url에 등록

--- account/urls.py
+++ account/urls.py
@@ -18,4 +18,10 @@ urlpatterns = [
     # path('edit/', views.edit, name='edit'),
     path('edit/', views.ProfileEditView.as_view(), name='edit'),
 
+
+    # path('users/', views.user_list, name='user_list'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    # path('users/<username>/', views.user_detail, name='user_detail'),
+    path('users/<username>/', views.UserDetailView.as_view(), name='user_detail'),
+
 ]
