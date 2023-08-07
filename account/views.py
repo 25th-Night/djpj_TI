2406ed181c33ac519a63d9b3ca5e8a69b6035390
25th-Night <brown_eyed87@naver.com>
2023-08-07 22:26:54 +0900

    account/views: dashboard 함수 로직 수정 - action 추가

--- account/views.py
+++ account/views.py
@@ -12,6 +12,7 @@ from django.views import View
 from django.views.decorators.http import require_POST
 from django.views.generic import FormView, TemplateView, ListView, DetailView
 
+from actions.models import Action
 from actions.utils import create_action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
@@ -117,7 +118,15 @@ def edit(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 class RegisterView(FormView):
