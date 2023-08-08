f53c71cb63e9e1618bb4c2f20543811997f58c29
25th-Night <brown_eyed87@naver.com>
2023-08-08 23:05:06 +0900

    cart/views: CartAddView FBV/CBV 추가

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+from django.views.decorators.http import require_POST
+from django.views.generic import FormView, DetailView
 
-# Create your views here.
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
+
+
+# class CartAddView(View):
+#
+#     def post(self, request, product_id):
+#         cart = Cart(request)
+#         product = get_object_or_404(Product, id=product_id)
+#         form = CartAddProductForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+#         return redirect('cart:cart_detail')
+
+
+class CartAddView(FormView):
+    form_class = CartAddProductForm
+
+    def form_valid(self, form):
+        cart = Cart(self.request)
+        product_id = self.kwargs['product_id']
+        product = get_object_or_404(Product, id=product_id)
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+        return redirect('cart:cart_detail')
