ad913fc35558d1f1e2a53c1454d5fb0d47b3ef9d
25th-Night <brown_eyed87@naver.com>
2023-08-08 21:25:36 +0900

    images/views: redis 비활성화

--- images/views.py
+++ images/views.py
@@ -16,9 +16,9 @@ from .forms import ImageCreateForm
 from .models import Image
 
 
-r = redis.Redis(host=settings.REDIS_HOST,
-                port=settings.REDIS_PORT,
-                db=settings.REDIS_DB)
+# r = redis.Redis(host=settings.REDIS_HOST,
+#                 port=settings.REDIS_PORT,
+#                 db=settings.REDIS_DB)
 
 
 @login_required
@@ -74,8 +74,9 @@ class ImageCreateView(LoginRequiredMixin, FormView):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    total_views = r.incr(f"image:{image.id}:views")
-    r.zincrby('image_ranking', 1, image.id)
+    # total_views = r.incr(f"image:{image.id}:views")
+    # r.zincrby('image_ranking', 1, image.id)
+    total_views=0
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
@@ -89,9 +90,9 @@ class ImageDetailView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         image = get_object_or_404(Image, id=self.kwargs['id'], slug=self.kwargs['slug'])
-        total_views = r.incr(f"image:{image.id}:views")
-        r.zincrby('image_ranking', 1, image.id)
-
+        # total_views = r.incr(f"image:{image.id}:views")
+        # r.zincrby('image_ranking', 1, image.id)
+        total_views=0
         context['section'] = 'images'
         context['image'] = image
         context['total_views'] = total_views
@@ -189,12 +190,11 @@ class ImageListView(LoginRequiredMixin, View):
 
 @login_required
 def image_ranking(request):
-    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
-    image_ranking_ids = [int(id) for id in image_ranking]
+    # image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    # image_ranking_ids = [int(id) for id in image_ranking]
+    image_ranking_ids = []
     most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
-    print(f"most_viewed 정렬 전: {most_viewed}")
     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
-    print(f"most_viewed 정렬 후: {most_viewed}")
     return render(request,
                   'images/ranking.html',
                   {'section': 'images',
@@ -206,8 +206,9 @@ class ImageRankingView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
-        image_ranking_ids = [int(id) for id in image_ranking]
+        # image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+        # image_ranking_ids = [int(id) for id in image_ranking]
+        image_ranking_ids = []
         most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
         most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
 
