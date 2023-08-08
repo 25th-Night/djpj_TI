92b94ba2641bde565d06a4e7d0f922846ce63559
25th-Night <brown_eyed87@naver.com>
2023-08-08 20:38:31 +0900

    config/settings: Redis 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -241,7 +241,12 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.user.user_details',
 ]
 
-
+# get_absolute_url 오버라이드
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+# Redis 설정
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
