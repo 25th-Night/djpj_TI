fc39e3c31f9daa9cf47a101e9a299048c05c3a0d
25th-Night <brown_eyed87@naver.com>
2023-08-08 22:47:44 +0900

    cart/cart: Cart 클래스에 add, save 메서드 추가

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 from django.conf import settings
+from shop.models import Product
 
 
 class Cart:
@@ -12,3 +14,20 @@ class Cart:
         # 세션에 빈 장바구니를 저장합니다.
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        제품을 장바구니에 추가하거나 수량을 업데이트 합니다.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
