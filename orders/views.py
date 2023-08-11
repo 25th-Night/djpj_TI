87ff9c0230c4cc95e8918bf539099e7de782d024
25th-Night <brown_eyed87@naver.com>
2023-08-12 07:51:35 +0900

    orders/views: OrderCrateView FBV/CBV 추가

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, FormView
 
-# Create your views here.
+from cart.cart import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
+
+
+def order_create(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
+
+
+class OrderCreateView(FormView):
+    form_class = OrderCreateForm
+    template_name = 'orders/order/create.html'
+
+    def get(self, request, *args, **kwargs):
+        cart = Cart(request)
+        form = self.get_form()
+        return self.render_to_response({'cart': cart, 'form': form})
+
+    def form_valid(self, form):
+        cart = Cart(self.request)
+        order = form.save()
+        for item in cart:
+            OrderItem.objects.create(
+                order=order,
+                product=item['product'],
+                price=item['price'],
+                quantity=item['quantity']
+            )
+
+        cart.clear()
+        return render(self.request, 'orders/created.html', {'order':order})
