3d4e8ee1ed2b43830cb42761073d360ee00543a3
25th-Night <brown_eyed87@naver.com>
2023-08-12 11:41:19 +0900

    config/celery: Celery app 설정

---
+++ config/celery.py
@@ -0,0 +1,12 @@
+import os
+
+from celery import Celery
+
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTING_MODULE', 'config.settings')
+
+app = Celery('myshop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
