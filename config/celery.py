7d82cdba1c9363c2bbf7efd02c560d6a7501c365
25th-Night <brown_eyed87@naver.com>
2023-08-12 12:02:57 +0900

    config/celery: 오타 수정

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTING_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('myshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
