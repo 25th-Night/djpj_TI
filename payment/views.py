b6891a2b2e9952149fba06358d9a3067d3683092
25th-Night <brown_eyed87@naver.com>
2023-08-17 21:49:55 +0900

    payment/views: PaymentProcessView 로직 추가 - Stripe session data의 line_items에 주문 제품 추가

--- payment/views.py
+++ payment/views.py
@@ -29,6 +29,19 @@ def payment_process(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
+
+        # add order items to Stripe session data.
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                },
+                'quantity': item.quantity,
+            })
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # redirect to Stripe payment form
@@ -66,6 +79,19 @@ class PaymentProcessView(View):
             'cancel_url': cancel_url,
             'line_items': []
         }
+
+        # add order items to Stripe session data.
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                },
+                'quantity': item.quantity,
+            })
 
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
