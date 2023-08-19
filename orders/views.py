afd5257b892167776005967ef8737ce7e4e32502
25th-Night <brown_eyed87@naver.com>
2023-08-20 08:27:54 +0900

    orders/views: admin_order_detail 뷰 함수 추가

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, FormView
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
-from orders.models import OrderItem
+from orders.models import OrderItem, Order
 
 from .tasks import order_created
 
@@ -63,3 +64,11 @@ class OrderCreateView(FormView):
         self.request.session['order_id'] = order.id
         # redirect for payment
         return redirect(reverse('payment:process'))
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                 'admin/orders/order/detail.html',
+                 {'order': order})
