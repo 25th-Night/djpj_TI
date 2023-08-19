fc7a1ed621a7146a103d7bd7cd1f22c39f7e6f5e
25th-Night <brown_eyed87@naver.com>
2023-08-20 08:51:14 +0900

    config/settings: STATIC_ROOT 설정

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # STATIC 경로
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # SECRET_KEY
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
