b9336c5dfcd3f9782f8c09b41b992c6d9c0e6fc7
25th-Night <brown_eyed87@naver.com>
2023-08-20 11:11:57 +0900

    cart/views: CartDetailView에 쿠폰 적용 폼 추가

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.views.decorators.http import require_POST
 from django.views.generic import FormView, DetailView
 
+from coupons.forms import CouponApplyForm
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -63,19 +64,32 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={
-            'quantity': item['quantity'],
-            'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+                                        'quantity': item['quantity'],
+                                        'override': True})
+    coupon_apply_form = CouponApplyForm()
+    return render(request,
+                  'cart/detail.html',
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form})
 
 
 class CartDetailView(DetailView):
+    model = Cart
     template_name = "cart/detail.html"
-    context_object_name = "cart"
 
-    def get_object(self):
-        cart = Cart(self.request)
+    def get_object(self, queryset=None):
+        return self.model(self.request)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = self.object
         for item in cart:
             item['update_quantity_form'] = CartAddProductForm(initial={
                 'quantity': item['quantity'],
                 'override': True})
-        return cart
+        coupon_apply_form = CouponApplyForm()
+        context.update({
+            'cart': cart,
+            'coupon_apply_form': coupon_apply_form
+        })
+        return context
