35cd9f7f553754b9abec92842898ecefe9d829ea
25th-Night <brown_eyed87@naver.com>
2023-09-01 00:02:00 +0900

    config/urls: rosetta 앱의 url 추가

--- config/urls.py
+++ config/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
+    path('rosetta/', include('rosetta.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
           name='django.contrib.sitemaps.views.sitemap'),
     path('__debug__/', include('debug_toolbar.urls')),
